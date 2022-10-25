964bd1c90f6ae37f13cc56c4173d68b1b87f5c03
Jules van Irsel <jules.van.irsel.gr@dartmouth.edu>
2022-10-25 16:13:30 -0400

    init can be used on pc or andes

--- init.m
+++ init.m
@@ -1,5 +1,11 @@
 % addpath('\\wsl$\Ubuntu-22.04\home\julesvanirsel\gemini\mat_gemini')
 % addpath('\\wsl$\Ubuntu-22.04\home\julesvanirsel\gemini\mat_gemini-scripts')
-addpath('D:\Files\research\gemini\mat_gemini')
-addpath('D:\Files\research\gemini\mat_gemini-scripts')
+if ispc
+    addpath('D:\Files\research\gemini\mat_gemini')
+    addpath('D:\Files\research\gemini\mat_gemini-scripts')
+else
+    addpath('//dartfs-hpc/rc/lab/L/LynchK/gemini/mat_gemini')
+    addpath('//dartfs-hpc/rc/lab/L/LynchK/gemini/mat_gemini-scripts')
+    setenv('EDITOR','vi')
+end
 setup
