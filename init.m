75d5cbc9915be58da783ceac0b738ba3943082a2
Jules van Irsel <jules.van.irsel.gr@dartmouth.edu>
2022-10-25 14:50:39 -0400

    Update init.m

--- init.m
+++ init.m
@@ -1,5 +1,5 @@
 % addpath('\\wsl$\Ubuntu-22.04\home\julesvanirsel\gemini\mat_gemini')
 % addpath('\\wsl$\Ubuntu-22.04\home\julesvanirsel\gemini\mat_gemini-scripts')
-addpath('D:\Files\research\thesis\gemini\mat_gemini')
-addpath('D:\Files\research\thesis\gemini\mat_gemini-scripts')
+addpath('D:\Files\research\gemini\mat_gemini')
+addpath('D:\Files\research\gemini\mat_gemini-scripts')
 setup
