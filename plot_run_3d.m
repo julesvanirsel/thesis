30503e1b1a7866f0e41bb43d3b7ef923a06ad8ee
Jules van Irsel <jules.van.irsel.gr@dartmouth.edu>
2022-12-19 12:37:45 -0500

    added options for different flux tube layouts

--- plot_run_3d.m
+++ plot_run_3d.m
@@ -1,13 +1,14 @@
-function plot_run_3d(direc,plots,alt_ref,lon_ref,options)
+function plot_run_3d(direc,plots,alt_ref,lon_ref,opts)
 arguments
     direc (1,:) char {mustBeFolder}
     plots (1,:) string
     alt_ref (1,1) double {mustBePositive}
     lon_ref (1,1) double {mustBeNumeric}
-    options.start (1,1) double {mustBeNumeric} = -1
-    options.cad (1,1) double {mustBeNumeric} = -1
-    options.stop (1,1) double {mustBeNumeric} = -1
-    options.dat (:,:) struct = struct
+    opts.start (1,1) double {mustBeNumeric} = -1
+    opts.cad (1,1) double {mustBeNumeric} = -1
+    opts.stop (1,1) double {mustBeNumeric} = -1
+    opts.dat (:,:) struct = struct
+    opts.plot_type (1,1) int8 = 1
 end
 
 % assertions
@@ -51,7 +52,6 @@ lz = length(z);
 dx = double(xg.dx2h);
 dy = double(xg.dx3h);
 dz = double(xg.dx1h(1:ubz));
-% [X,Y,~] = ndgrid(x,y,z);
 [Xm,Ym,Zm] = meshgrid(x,y,z);
 [dX,dY,dZ] = ndgrid(dx,dy,dz);
 dV = dX.*dY.*dZ;
@@ -64,20 +64,20 @@ tdur = cfg.tdur;
 dtout = cfg.dtout;
 
 %% setting time boundaries
-if options.start < 0
+if opts.start < 0
     start = 0;
 else
-    start = options.start;
+    start = opts.start;
 end
-if options.cad <= 0
+if opts.cad <= 0
     cad = dtout;
 else
-    cad = options.cad;
+    cad = opts.cad;
 end
-if options.stop < start
+if opts.stop < start
     stop = tdur;
 else
-    stop = options.stop;
+    stop = opts.stop;
 end
 
 %% main loop
@@ -85,10 +85,10 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
     %% loading simulation data
     time = datetime(ymd) + seconds(UTsec);
     time.Format = 'yyyyMMdd''T''HHmmss.SSS';
-    if isempty(fields(options.dat))
+    if isempty(fields(opts.dat))
         dat = gemini3d.read.frame(direc,'time',time);
     else
-        dat = options.dat;
+        dat = opts.dat;
     end
     title_time = char(dat.time);
     filename_prefix = [char(time),'UT'];
@@ -97,7 +97,6 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
     %% formatting simulation data
     phi = dat.Phitop;
     [Ez,Ex,Ey] = gemscr.postprocess.pot2field(xg,phi);
-    %     [sigP,sigH,~,~] = load_conductances(direc,time,dat,cfg,xg);
 
     Ex = permute(Ex(1:ubz,:,:),[2,3,1]);
     Ey = permute(Ey(1:ubz,:,:),[2,3,1]);
@@ -106,20 +105,17 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
     jy = permute(dat.J3(1:ubz,:,:),[2,3,1]);
     jz = permute(dat.J1(1:ubz,:,:),[2,3,1]);
     ne = permute(dat.ne(1:ubz,:,:),[2,3,1]);
-    %     sigP = permute(sigP(1:ubz,:,:),[2,3,1]);
-    %     sigH = -permute(sigH(1:ubz,:,:),[2,3,1]);
 
     % implicit simulation data
     joule = jx.*Ex + jy.*Ey + jz.*Ez;
 
     % set data ranges
-    j1_range = [-1,1]*quantile(abs(jz(:)),qnt);
+    j1_range = [-1,1]*quantile(abs(jz(:,:,end)),qnt,'all');
 
     %% plotting routines
     set(0,'defaultTextFontSize',fts)
 
     jz_p = -jz*j_scl;
-    %     sigP_p = sigP*s_scl; sigH_p = sigH*s_scl;
     ne_p = ne*n_scl;
     Xm_p = Xm*x_scl; Ym_p = Ym*x_scl; Zm_p = Zm*x_scl;
 
@@ -133,23 +129,36 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
 
     % flux tube plot
     if ismember('fluxtubes',plots)
-        folder = 'fluxtubes';
+        folder = ['fluxtubes',num2str(opts.plot_type)];
         suffix = 'flux';
         folder_suffix = {'iso','side','top'};
-%         p0 = [[lon_ref_p,40,300];[lon_ref_p,110,300];[lon_ref_p,180,300]]*1e3*x_scl;
-%         v0 = [[1,0,0];[1,0,0];[1,0,0]];
-        p0 = [[lon_ref_p,40,300];[lon_ref_p,80,300];[1000,100,115]]*1e3*x_scl;
-        v0 = [[1,0,0];[1,0,0];[0,0,1]];
-        r0 = [200,200,10]*1e3*x_scl;
-        r1 = [20,20,45]*1e3*x_scl;
-        ntubes = length(p0);
-%         r0 = ones(1,ntubes)*200e3*x_scl;
-%         r1 = ones(1,ntubes)*20e3*x_scl;
-        colors = [[1.0, 0.5, 0.0];...
-            [0.2, 0.8, 0.2];...
-            [0.8, 0.1, 0.8]
-            [1.0, 1.0, 1.0]...
-            ];
+
+        if opts.plot_type == 1
+            ntubes = 3;
+            p0 = [[lon_ref_p,40,300];[lon_ref_p,110,300];[lon_ref_p,180,300]]*1e3*x_scl;
+            v0 = [[1,0,0];[1,0,0];[1,0,0]];
+            r0 = ones(1,ntubes)*200e3*x_scl;
+            r1 = ones(1,ntubes)*20e3*x_scl;
+            colors = [...
+                [1.0, 0.5, 0.0];...
+                [0.2, 0.2, 0.8];...
+                [0.0, 0.5, 0.0];...
+                ];
+            res = [64,64,64];
+        elseif opts.plot_type == 2
+            ntubes = 3;
+            p0 = [[lon_ref_p,40,300];[lon_ref_p,160,300];[1000,100,115]]*1e3*x_scl;
+            v0 = [[1,0,0];[1,0,0];[0,0,1]];
+            r0 = [200,200,10]*1e3*x_scl;
+            r1 = [20,20,45]*1e3*x_scl;
+            colors = [...
+                [1.0, 0.5, 0.0];...
+                [0.2, 0.2, 0.8];...
+                [1.0, 0.0, 0.0]...
+                ];
+            res = [64,64,32];
+        end
+
         views = [[30,45];[90,0];[0,90]];
         dviews = [[10*2*(UTsec0-UTsec+tdur/2)/tdur,0];[0,0];[0,0]];
         paper_w = [8,11,9.5];
@@ -159,7 +168,7 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
         % call fluxtube prior for speed
         tubes = struct;
         for n = 1:ntubes
-            tubes.(char(64+n)) = fluxtube(xg,dat,alt_ref*x_scl,p0(n,:),r0(n),r1(n),v0=v0(n,:),reverse=1,calculate_hull=1,res=64);
+            tubes.(char(64+n)) = fluxtube(xg,dat,alt_ref*x_scl,p0(n,:),r0(n),r1(n),v0=v0(n,:),reverse=1,calculate_hull=1,res=res(n));
         end
         for v = 1:length(views)
             vv = views(v,:)+dviews(v,:);
@@ -195,6 +204,7 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
             slice(axj,Xm_p,Ym_p,Zm_p,permute(jz_p,[2,1,3]),[],[],alt_ref_p);
             colormap(axj,colorcet(clm.j))
             shading(axj,'flat')
+            clim(axj,j1_range_p)
             if v == 2
                 clb = colorbar(axj);
                 clb.Label.String = ['j_{||} [',units.j,']'];
@@ -203,7 +213,6 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
                 clb.Ruler.TickLabelFormat = clb_fmt;
                 clb.Ruler.Exponent = clb_exp;
             end
-            clim(axj,j1_range_p)
 
             slice(axn,Xm_p,Ym_p,Zm_p,permute(log10(ne_p),[2,1,3]),lon_ref_p,[],[]);
             colormap(axn,colorcet(clm.n))
@@ -238,22 +247,22 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
                 pl0 = plot3(axt,c0(:,1),c0(:,2),c0(:,3));
                 pl1 = plot3(axt,c1(:,1),c1(:,2),c1(:,3));
                 stl = streamline(axt,verts);
-                if n~=3
-                shd = slice(axj,Xm_p,Ym_p,Zm_p,permute(shadow,[2,1,3]),[],[],alt_ref_p);
+                if any(not(isnan(shadow,'all')))
+                    shd = slice(axj,Xm_p,Ym_p,Zm_p,permute(shadow,[2,1,3]),[],[],alt_ref_p);
                 end
-                plot3(axt,[1,1,1]*lon_ref_p,[ylims_p,ylims_p(2)],[zlims_p(1),zlims_p],'--','Color',[0,1.0,0]);
-                plot3(axt,[xlims_p(1),xlims_p],[ylims_p,ylims_p(2)],[1,1,1]*alt_ref_p,'--','Color',[0,0,1.0]);
+                plot3(axt,[1,1,1]*lon_ref_p,[ylims_p,ylims_p(2)],[zlims_p(1),zlims_p],'--','Color',[0,1,0]);
+                plot3(axt,[xlims_p(1),xlims_p],[ylims_p,ylims_p(2)],[1,1,1]*alt_ref_p,'--','Color',[0,0,1]);
 
                 shading(axj,'flat')
                 set(pl0,'Color','k','LineWidth',1)
                 set(pl1,'Color',color,'LineWidth',1)
                 set(shd,'FaceAlpha',0.5)
                 set(stl,'Color',[color,0.5],'LineWidth',1)
-
-                xlabel(['East [',units.x,']'],'FontSize',fts)
-                ylabel(['North [',units.x,']'],'FontSize',fts)
-                zlabel(['Up [',units.x,']'],'FontSize',fts)
             end
+
+            xlabel(['East [',units.x,']'],'FontSize',fts)
+            ylabel(['North [',units.x,']'],'FontSize',fts)
+            zlabel(['Up [',units.x,']'],'FontSize',fts)
 
             flux_strings = cell(2,ntubes);
             heat_strings = cell(1,ntubes);
@@ -270,7 +279,7 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
                     'Current In:  ',cell2mat(flux_strings(1,:)),newline,...
                     'Current Out:  ',cell2mat(flux_strings(2,:)),newline,...
                     'Joule Heating:  ',cell2mat(heat_strings),...
-                    ],'FitBoxToText','on','EdgeColor','none','BackgroundColor','none','FontSize',0.7*fts)
+                    ],'FitBoxToText','on','EdgeColor','none','BackgroundColor','none','FontSize',0.6*fts)
             end
 
             full_folder = [folder,'_',cell2mat(folder_suffix(v))];
@@ -278,6 +287,7 @@ for UTsec = UTsec0+start:cad:UTsec0+stop
                 mkdir(direc,fullfile('plots_3d',full_folder));
             end
             saveas(gcf,fullfile(direc,'plots_3d',full_folder,[filename_prefix,'_',suffix,'.png']))
+            disp(fullfile(direc,'plots_3d',full_folder,[filename_prefix,'_',suffix,'.png']))
             close all
         end
     end
