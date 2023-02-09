5c59486e99e1620bfaf05ae7d9ed5cab25049136
Jules van Irsel <jules.van.irsel.gr@dartmouth.edu>
2023-02-09 16:07:27 -0500

    in progress

--- density2precip.m
+++ density2precip.m
@@ -1,18 +1,46 @@
-runs = struct2cell(dir('./runs/aurora_*'));
+%% generate simulation inputs
+% nE0 = 10;
+% nQ = 10;
+% [E0g,Qg] = ndgrid(10.^linspace(2.001,4,nE0),10.^linspace(-1,2,nQ));
+% 
+% for i = 1:nE0
+%     for j = 1:nQ
+%         E0_tmp = E0g(i,j);
+%         Q_tmp = Qg(i,j);
+%         runname = ['pt_',num2str(E0_tmp,'%1.2e'),'_',num2str(Q_tmp,'%1.2e')];
+%         run_series('runs\preciptest\aurora_preciptest_base\',runname,["W0BG","PhiWBG"],[E0_tmp;Q_tmp])
+%     end
+% end
+% fclose all;
+
+nE0 = 5;
+nQ = 5;
+[E0g,Qg] = ndgrid(10.^linspace(2.001,4,nE0),10.^linspace(-1,2,nQ));
+
+for i = 1:nE0
+    for j = 1:nQ
+        E0_tmp = E0g(i,j);
+        Q_tmp = Qg(i,j);
+        runname = ['pt_',num2str(E0_tmp,'%1.2e'),'_',num2str(Q_tmp,'%1.2e')];
+        run_series('runs\preciptest_fang2010\aurora_preciptest_base\',runname,["W0BG","PhiWBG"],[E0_tmp;Q_tmp])
+    end
+end
+fclose all;
+
+%% make data matrix
+runs = struct2cell(dir('.\runs\preciptest\pt_*'));
 runs = runs(1,:);
 lruns = length(runs);
 
 time = datetime(2015,2,1,0,0,36000);
-folder = 'D:\Files\research\thesis\runs\';
+folder = 'D:\Files\research\thesis\runs\preciptest\';
 direc = fullfile(folder,runs{1});
 xg = gemini3d.read.grid(direc);
 alt = xg.x1(3:end-2)/1e3;
 
-%%
-doplots = 1;
-
 clm = colorcet('D1A');
 data = nan(lruns,4);
+ne_data = nan(lruns,length(alt));
 for run = 1:lruns
     disp(['run: ',num2str(run)])
     direc = fullfile(folder,runs{run});
@@ -27,70 +55,66 @@ for run = 1:lruns
             E0 = str2double(s(8:end-17))/1e3;
         end
     end
-    
     data(run,1:2) = [E0,Q];
 
     try
         dat = gemini3d.read.frame(direc,'time',time,'var','ne');
         ne = squeeze(dat.ne(:,round(end/2)));
+        ne_data(run,:) = log10(ne);
         [~,i] = max(ne);
         data(run,3:4) = [alt(i),log10(ne(i))];
     catch
         disp(['SKIPPED: ',direc])
     end
-
-    if doplots && run>20 && run<31
-        cl = clm(round(((run-20)/10)*255+1),:);
-        figure(1)
-        hold on
-        plot(log10(ne),alt,'color',cl)
-        ylim([80,300])
-        yline(alt(i),'color',cl)
-    end
-    if doplots && run>30
-        cl = clm(round(((run-30)/10)*255+1),:);
-        figure(2)
-        hold on
-        plot(log10(ne),alt,'color',cl)
-        ylim([80,300])
-        yline(alt(i),'color',cl)
-    end
 end
 
-%% get Q from ne at max alt
-Qf = fit(data(1:20,4),log10(data(1:20,2)),'poly1');
-nemax = linspace(10,13,10);
-
-hold on
-scatter(data(1:10,4),log10(data(1:10,2)),'r')
-scatter(data(11:20,4),log10(data(11:20,2)),'b')
-plot(nemax,Qf(nemax),'k')
-xlabel('log_{10} n_{e,max} [m^{-3}]')
-ylabel('log_{10} Q [mW/m^2]')
-xlim([10,13])
-ylim([-2,3])
-legend( ...
-    ['E_0 = ', num2str(data(1,1)), ' keV'] ...
-    ,['E_0 = ', num2str(data(11,1)), ' keV'] ...
-    ,'Location','northwest'...
-    )
-
-%% get E0 from Q and max alt
-hold on
-scatter(log10(data(21:30,1)),data(21:30,3),'r')
-scatter(log10(data(31:40,1)),data(31:40,3),'b')
-legend( ...
-    ['Q = ', num2str(data(21,2)), ' keV'] ...
-    ,['Q = ', num2str(data(31,2)), ' keV'] ...
-    ,'Location','northwest'...
-    )
+% order data set
+i = 1;
+data_ord = nan(size(data));
+for E0 = unique(data(:,1))'
+    ids = data(:,1)==E0;
+    data_tmp = data(ids,:);
+    [~,o] = sort(data_tmp(:,2));
+    ld = length(data_tmp);
+    data_ord(i:ld+i-1,:) = data_tmp(o,:);
+    i = i+ld;
+end
+data = data_ord;
 
 %%
-% direc=['//Dartfs-hpc/rc/lab/L/LynchK/public_html/pubdata/2021_clayton_jgr/data/Simulations/c5' ...
-% direc = './20170302_28365.000000.dat';                % folder where model output data is located
-% direc = './runs/c5';
-% time = datetime([2017,03,02,0,0,28365]);        % what time is of interest
-% xg = gemini3d.read.grid(direc);   % from mat_gemini, reads in the grid info
+ids = not(isnan(data(:,3))) & data(:,3)<250 & data(:,4)>10;
+
+E0sf = fit([data(ids,3),data(ids,4)],log10(data(ids,1)),'poly33');
+Qsf = fit([data(ids,3),data(ids,4)],log10(data(ids,2)),'poly22');
+
+fs = 12;
+
+figure(1)
+plot(E0sf,[data(ids,3),data(ids,4)],log10(data(ids,1)))
+alpha 0.5
+set(gca,'FontSize',fs)
+xlabel('Altitude of Peak Electron Density [km]')
+ylabel('log_{10} Peak Electron Density [m^{-3}]')
+zlabel('log_{10} Average Energy [keV]')
+
+figure(2)
+plot(Qsf,[data(ids,3),data(ids,4)],log10(data(ids,2)))
+alpha 0.5
+set(gca,'FontSize',fs)
+xlabel('Altitude of Peak Electron Density [km]')
+ylabel('log_{10} Peak Electron Density [m^{-3}]')
+zlabel('log_{10} Total Energy Flux [mW/m^2]')
 
 %%
-% simdat = gemini3d.read.frame(direc, "time", time); % reads in the model data
+clmi = round(linspace(1,256,10));
+tiledlayout(2,5)
+
+for i=1:10
+    nexttile
+    data_tmp = data(10*(i-1)+1:10*(i-1)+10,:);
+    ne_data_tmp = ne_data(10*(i-1)+1:10*(i-1)+10,:);
+    hold on
+    for j=1:10
+        plot(ne_data_tmp(j,:),alt,'Color','k')
+    end
+end
