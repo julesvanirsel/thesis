b514fb295b8489158dbb638d4864a205fcc33186
julesvanirsel <jules.van.irsel.gr@dartmouth.edu>
2024-05-26 17:08:55 -0400

    added geodetic coordinates

--- cdf_to_h5.m
+++ cdf_to_h5.m
@@ -1,5 +1,6 @@
-direc = fullfile('data','swop','swarm_cdfs');
+direc = fullfile('data','swop','swarm_data');
 cdfs = {dir(fullfile(direc,'*.cdf')).name};
+
 for cdf = cdfs
     path = fullfile(direc,cdf{1});
     path_h5 = [path(1:end-3),'h5'];
@@ -8,6 +9,7 @@ for cdf = cdfs
     units = info.VariableAttributes.UNITS(:,2);
     types = info.VariableAttributes.Type(:,2);
     descs = info.VariableAttributes.CATDESC(:,2);
+
     for i = 1:length(vars)
         data = [all_data{:,i}];
         var = ['/',vars{i}];
@@ -25,13 +27,31 @@ for cdf = cdfs
             unit = 's';
             desc = 'Unix time.';
         end
-        h5create(path_h5,var,size(data),'Datatype',type)
-        h5write(path_h5,var,data)
-        h5writeatt(path_h5,var,'Units',unit)
-        h5writeatt(path_h5,var,'Description',desc)
+        try
+            h5create(path_h5,var,size(data),'Datatype',type)
+            h5write(path_h5,var,data)
+            h5writeatt(path_h5,var,'Units',unit)
+            h5writeatt(path_h5,var,'Description',desc)
+        catch
+            warning('%s in %s already exists',var(2:end),path_h5)
+        end
+
+        if strcmp(var,'/Latitude'); lat_gc = data; end
+        if strcmp(var,'/Radius'); rad = data; end
     end
+    
     global_atts = fieldnames(info.GlobalAttributes)';
     for att = global_atts
         h5writeatt(path_h5,'/',att{1},info.GlobalAttributes.(att{1}))
     end
+
+    [lat_gd,alt_gd] = geoc2geod(lat_gc,rad);
+    h5create(path_h5,'/GeodeticLatitude',size(lat_gd),'Datatype','double')
+    h5write(path_h5,'/GeodeticLatitude',lat_gd)
+    h5writeatt(path_h5,'/GeodeticLatitude','Units','degrees')
+    h5writeatt(path_h5,'/GeodeticLatitude','Description','Geodetic Latitude.')
+    h5create(path_h5,'/GeodeticAltitude',size(alt_gd),'Datatype','double')
+    h5write(path_h5,'/GeodeticAltitude',alt_gd)
+    h5writeatt(path_h5,'/GeodeticAltitude','Units','m')
+    h5writeatt(path_h5,'/GeodeticAltitude','Description','Geodetic Altitude.')
 end
