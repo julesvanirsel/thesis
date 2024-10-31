f46856793499f4ab4d409efcfd22ca2a48a04c4c
julesvanirsel <julesvanirsel@gmail.com>
2024-10-31 12:06:26 -0400

    added glow tools

--- +jules/+glow/generate_input.m
+++ +jules/+glow/generate_input.m
@@ -1,20 +1,25 @@
-function filename = generate_input(date,glat,glon)
+function filename = generate_input(date, glat, glon, opts)
 arguments
-    date (1,1) datetime
-    glat (1,1) double {mustBeNonempty}
-    glon (1,1) double {mustBeNonempty}
+    date (1, 1) datetime
+    glat (1, 1) double {mustBeNonempty}
+    glon (1, 1) double {mustBeNonempty}
+    opts.do_acc (1, 1) logical = false
+    opts.thermal_energy_ev (1, 1) double = 0;    
 end
 
 date.Format = 'yyDDD';
 glat = mod(glat + 90, 181) - 90;
 glon = mod(glon, 360);
 [f107, f107p, f107a, Ap] = jules.tools.activity(date);
-Ec = 1; % not used
-Q = 1000; % not used
+% Ec = 1; % not used
+% Q = 1000; % not used
 filename = sprintf('in.invert.%s_%i', date, second(date, 'secondofday'));
+if opts.do_acc
+    filename = [filename, '_acc'];
+end
 
 f = fopen(fullfile('glow', 'input', filename), 'w');
-fprintf(f, '%s %5i %5.2f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f\n', ...
-    date, second(date, 'secondofday'), glat, glon, f107a, f107, f107p, Ap, Ec, Q);
+fprintf(f, '%s %5i %5.2f %6.1f %6.1f %6.1f %6.1f %6.1f %1i %7.1f\n', ...
+    date, second(date, 'secondofday'), glat, glon, f107a, f107, f107p, Ap, opts.do_acc, opts.thermal_energy_ev);
 
 fclose all;
--- +jules/+glow/generate_invert_table.m
+++ +jules/+glow/generate_invert_table.m
@@ -1,16 +1,19 @@
 function generate_invert_table(date, glat, glon, opts)
 arguments
-    date (1,1) datetime
-    glat (1,1) double {mustBeNonempty}
-    glon (1,1) double {mustBeNonempty}
-    opts.version (1,1) int32 {mustBePositive} = 3
-    opts.out_direc (1,:) char {mustBeFolder} = '.'
+    date (1, 1) datetime
+    glat (1, 1) double {mustBeNonempty}
+    glon (1, 1) double {mustBeNonempty}
+    opts.version (1, 1) int32 {mustBePositive} = 3
+    opts.out_direc (1, :) char {mustBeFolder} = '.'
+    opts.do_acc (1, 1) logical = false
+    opts.thermal_energy_ev (1, 1) double = 0;   
 end
 
 assert(not(ispc), 'Please run this function in a Linux environment.')
 
-ver = sprintf('v%i',opts.version);
-in_filename = jules.glow.generate_input(date, glat, glon);
+ver = sprintf('v%i', opts.version);
+in_filename = jules.glow.generate_input(date, glat, glon, ...
+    do_acc=opts.do_acc, thermal_energy_ev=opts.thermal_energy_ev);
 in_path = fullfile('input', in_filename);
 
 for airglow = [true, false]
@@ -36,14 +39,14 @@ for airglow = [true, false]
 
     if airglow
         fprintf('Generating airglow inversion value.\n')
-        command = fullfile('.',sprintf('glow_invert_airglow_%s.exe < %s', ver, in_path));
+        command = fullfile('.', sprintf('glow_invert_airglow_%s.exe < %s', ver, in_path));
     else
         fprintf('Generating inversion tables.\n')
-        command = fullfile('.',sprintf('glow_invert_tables_%s.exe < %s', ver, in_path));
+        command = fullfile('.', sprintf('glow_invert_tables_%s.exe < %s', ver, in_path));
     end
     system(sprintf('cd glow; %s', command));
     
-    bins = {dir(fullfile(glow_out_path,'*.bin')).name};
+    bins = {dir(fullfile(glow_out_path, '*.bin')).name};
     for bin = bins
         movefile(fullfile(glow_out_path, bin{1}), out_direc, 'f')
     end
